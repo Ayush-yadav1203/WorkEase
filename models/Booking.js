e2bbcfa5ad1eebd47be298ff68f3aab7be479c6d
AYUSH YADAV <raosahab.ay1203@gmail.com>
2026-02-23 18:43:26 +0530

    new booking. js

--- models/Booking.js
+++ models/Booking.js
@@ -1,12 +1,27 @@
 const mongoose = require("mongoose");
 
-const BookingSchema = new mongoose.Schema({
-  name: String,
-  phone: String,
-  service: String,
-  date: String,
-  time: String,
-  createdAt: { type: Date, default: Date.now }
-});
+const bookingSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  service: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
 
-module.exports = mongoose.model("Booking", BookingSchema);
+module.exports = mongoose.model("Booking", bookingSchema);
